c83dd7231d1fc41a4a755d5b0770184caf18770f
RuanSampaio-code <ruansampaio001@gmail.com>
2022-06-30 09:57:39 -0300

    anotações js

---
+++ Introdução ao Javascript/02function.js
@@ -0,0 +1,6 @@
+﻿function soma(a,b){
+	console.log(a+b);
+	return a + b; // retorna valor de a + b
+}
+
+soma(3,5);
---
+++ Introdução ao Javascript/03teste.js
@@ -0,0 +1,13 @@
+﻿function returnEvenValues(array){
+    let evenNums = [];
+    for (let i = 0; i < array.length; ) {
+        if (array[i]%2 ===0){
+            evenNums.push(array[i])
+        }
+   }
+   console.log(evenNums)
+}
+
+let array = [1,2,4,5,7,9];
+returnEvenValues(array);
+
