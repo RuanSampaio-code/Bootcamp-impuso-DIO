6da8fa90504fc95ccc9b0e0db527c6802ecbb5a6
RuanSampaio-code <ruansampaio001@gmail.com>
2022-07-04 20:20:07 -0300

    projeto contador

--- Introdução ao Javascript/03teste.js
+++ Introdução ao Javascript/03teste.js
@@ -1,11 +1,13 @@
 ﻿function returnEvenValues(array){
     let evenNums = [];
-    for (let i = 0; i < array.length; ) {
+    for (let i = 0; i < array.length; i++ ) {
         if (array[i]%2 ===0){
-            evenNums.push(array[i])
+            evenNums.push(array[i]);
+        } else{
+            console.log("Estes numeros não são pares", array[i]);
         }
    }
-   console.log(evenNums)
+   console.log('os numeros pares são',evenNums);
 }
 
 let array = [1,2,4,5,7,9];
